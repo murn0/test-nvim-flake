29b3a5a1d1612303ad0ffd1ce4938bfedccac873
murn0 <47878863+murn0@users.noreply.github.com>
2024-03-08 02:25:50 +0900

    update flake.

--- flake.nix
+++ flake.nix
@@ -4,17 +4,11 @@
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    neovim-nightly-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     neovim-flake = { url = "github:neovim/neovim?dir=contrib"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "aarch64-darwin"
         "x86_64-linux"
@@ -24,71 +18,58 @@
         inputs.flake-parts.flakeModules.easyOverlay
       ];
 
-      perSystem = { config, inputs', lib, pkgs, system, ... }: {
-        # _module.args.pkgs = import inputs.nixpkgs {
-        #   inherit system;
-        #   overlays = [
-        #     inputs.neovim-nightly-overlay.overlay
-        #   ];
-        # };
+      perSystem = { config, inputs', lib, pkgs, system, ... }:
+      let
+        runtimeDeps = with pkgs; [
+          # Tree sitter
+          git
+        ];
 
-
-        # formatter = pkgs.alejandra;
-
-        packages.default = config.packages.neovim;
-        packages.neovim = let
-          neovim-unwrapped = inputs'.neovim-flake.packages.neovim;
-
-          config-dir = pkgs.symlinkJoin {
+        configDir = pkgs.symlinkJoin {
+          name = "neovim-config-dir";
+          paths = (builtins.path {
             name = "neovim-config-dir-src";
-            paths = (builtins.path {
-              name = "neovim-config-dir-src";
-              path = ./.;
-              filter = path: type: type == "directory" || lib.hasSuffix ".lua" path;
-            });
-          };
-
-          neovim-with-plugins = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (
-            (pkgs.neovimUtils.makeNeovimConfig {
-                withPython3 = false;
-                withNodeJs = true;
-                withRuby = false; 
-            }) // {
-              wrapperArgs = "";
-              wrapRc = false;
-            });
-        in
-        pkgs.stdenvNoCC.mkDerivation {
-          name = "neovim";
-
-          buildInputs = [
-            pkgs.makeWrapper
-          ];
-
-          dontBuild = true;
-          dontUnpack = true;
-
-          installPhase = ''
-            runHook preInstall
-
-            mkdir -p $out/bin
-            makeWrapper ${neovim-with-plugins}/bin/nvim $out/bin/nvim \
-              --add-flags "--cmd 'lua vim.opt.runtimepath:append(\"${config-dir}\")'" \
-              --add-flags "--clean" \
-              --add-flags "-u ${config-dir}/init.lua"
-
-            runHook postInstall
-          '';
-
-          meta = {
-            mainProgram = "nvim";
-          };
+            path = ./.;
+            filter = path: type: type == "directory" || lib.hasSuffix ".lua" path;
+          });
         };
 
+        cfg = let
+          neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
+            withPython3 = false;
+            withNodeJs = true;
+            withRuby = false;
+            wrapRc = false;
+          };
+        in 
+          neovimConfig // {
+            wrapperArgs = 
+              neovimConfig.wrapperArgs ++
+              [
+                "--prefix"
+                "PATH"
+                ":"
+                "${lib.makeBinPath runtimeDeps}"
+              ] ++
+              [
+                "--add-flags"
+                ''--cmd "lua vim.opt.runtimepath:append(\"${configDir}\")"''
+                "--add-flags"
+                "--clean"
+                "--add-flags"
+                ''-u ${configDir}/init.lua''
+              ];
+          };
+      in
+      {
+        packages.default = config.packages.neovim;
+        packages.neovim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (cfg);
+        packages.nightly = pkgs.wrapNeovimUnstable inputs'.neovim-flake.packages.neovim (cfg);
+
         overlayAttrs = {
-          inherit (config.packages) neovim;
+          inherit (config.packages) neovim nightly;
         };
 
       };
-};
+    };
 }
